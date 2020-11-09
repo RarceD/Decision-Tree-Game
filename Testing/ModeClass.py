2a39d4faede6cf990b9567d046c761c3e4e505a9
RarceD <akimbo170@gmail.com>
2020-11-09 18:40:32 +0100

    create the json input parser for the dumiest teacher

---
+++ Testing/ModeClass.py
@@ -0,0 +1,16 @@
+class Mode:
+    def __init__(self, name):
+        self.name = name
+        self.images = []
+        self.words_right = []
+        self.words_wrong = []
+    def random_word(self):
+        pass
+    def print_itself(self):
+        print("Mode: "+ self.name)
+        print(" -> Images: ", self.images)
+        print(" -> Correct words: ", self.words_right)
+        print(" -> Incorrect words: ", self.words_wrong)
+
+
+
--- Testing/input_parameters.py
+++ Testing/input_parameters.py
@@ -1,10 +1,14 @@
 import json
+from ModeClass import Mode
 
+modes = []
 with open('input.json') as json_file:
     data = json.load(json_file)
-    for p in data['modes']:
-        print('Name: ' + p['name'])
-        print('Website: ' + p['from'])
-        print('From: ' + p['clear'])
-        print('')
-    print(data['hola'])
+    for index, p in enumerate(data['modes']):
+        modes.append(Mode(p['name']))
+        modes[index].words_right.append( p["words_right"])
+        modes[index].words_wrong.append(p["words_wrong"])
+        modes[index].images.append(p["images"])
+
+for m in modes:
+    m.print_itself()
